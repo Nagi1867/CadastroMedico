a97667e2e5964c51f8ddcd4497a2133524cfbc3b
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-09 14:36:25 -0300

    projeto criado

---
+++ src/main/java/com/example/CadastroMedico/CadastroMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CadastroMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CadastroMedico/CadastroMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CadastroMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
